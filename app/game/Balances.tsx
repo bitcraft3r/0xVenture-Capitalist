637acf3ec7ba19a4d365a28e7ce9936ea33c47ca
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-05-01 14:38:42 +0800

    fix: update coin balance to show 2 decimal

--- app/game/Balances.tsx
+++ app/game/Balances.tsx
@@ -6,7 +6,7 @@ const Balances: React.FC<BalancesProps> = ({ coins }) => {
     return (
         <div className="w-[80vw] h-[10vh] border mb-[2rem]">
             Balances
-            <div>Coins: ${coins}</div>
+            <div>Coins: ${coins.toLocaleString('en-US', { minimumFractionDigits: 2 })}</div>
         </div>
     )
 }
