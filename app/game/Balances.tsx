986b7247c9d50d5529746e719b6189f2fcd33824
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-05-01 23:49:49 +0800

    feat: add mascot img, Balances bar design +buttons

--- app/game/Balances.tsx
+++ app/game/Balances.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useStore } from "@/app/store/GameStore"
+import Image from "next/image"
 import { useEffect } from "react"
 
 interface BalancesProps {
@@ -21,9 +22,16 @@ const Balances: React.FC<BalancesProps> = ({ coins }) => {
     }, [])
 
     return (
-        <div className="w-[80vw] h-[10vh] border mb-[2rem]">
-            Balances
-            <div>Coins: ${userCoins?.toLocaleString('en-US', { minimumFractionDigits: 2 })}</div>
+        <div className="h-[120px] border mb-[2rem]">
+            <div className="flex justify-between items-center">
+                <Image src="/images/mascot.png" alt="mascot" height="120" width="120" />
+                <div className="text-3xl font-bold">${userCoins?.toLocaleString('en-US', { minimumFractionDigits: 2 })}</div>
+                <div className="flex flex-col">
+                    <button className="border rounded-lg px-4 py-1 mr-[1rem] my-1">Managers</button>
+                    <button className="border rounded-lg px-4 py-1 mr-[1rem] my-1">Upgrades</button>
+
+                </div>
+            </div>
         </div>
     )
 }
