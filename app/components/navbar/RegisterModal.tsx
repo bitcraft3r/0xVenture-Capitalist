6f7451ccf5859c469e948a2f06e77fcee3a4af7d
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-05-08 20:56:51 +0800

    chore: update game name

--- app/components/navbar/RegisterModal.tsx
+++ app/components/navbar/RegisterModal.tsx
@@ -12,7 +12,7 @@ const RegisterModal = ({ children }: { children: React.ReactNode }) => (
             <Dialog.Overlay className="bg-blackA9 data-[state=open]:animate-overlayShow fixed inset-0 z-20" />
             <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none z-30">
                 <Dialog.Title className="text-mauve12 m-0 text-[17px] font-medium">
-                    Welcome to Blockchain Billionaire
+                    Welcome to 0xVenture Capitalist
                 </Dialog.Title>
                 <Dialog.Description className="text-mauve11 mt-[10px] mb-5 text-[15px] leading-normal">
                     Sign in or create an account to get started!
