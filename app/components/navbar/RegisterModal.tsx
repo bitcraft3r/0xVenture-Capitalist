a42175ffebb80aa7acd27ed62a8ada1486fdc253
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-05-12 15:13:20 +0800

    style: register/signin modal with theme colors

--- app/components/navbar/RegisterModal.tsx
+++ app/components/navbar/RegisterModal.tsx
@@ -3,6 +3,8 @@ import * as Dialog from '@radix-ui/react-dialog';
 import { Cross2Icon, GitHubLogoIcon, TwitterLogoIcon } from '@radix-ui/react-icons';
 import { FcGoogle } from 'react-icons/fc';
 
+const commonButtonClass = "bg-neutral-100 border text-neutral-800 hover:bg-white hover:border-gray-400 hover:shadow-lg inline-flex h-[35px] items-center justify-center rounded-[4px] px-[15px] font-medium leading-none w-full text-sm focus:bg-neutral-200 focus:shadow-xl"
+
 const RegisterModal = ({ children }: { children: React.ReactNode }) => (
     <Dialog.Root>
         <Dialog.Trigger asChild>
@@ -10,16 +12,18 @@ const RegisterModal = ({ children }: { children: React.ReactNode }) => (
         </Dialog.Trigger>
         <Dialog.Portal>
             <Dialog.Overlay className="bg-blackA9 data-[state=open]:animate-overlayShow fixed inset-0 z-20" />
-            <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none z-30">
-                <Dialog.Title className="text-mauve12 m-0 text-[17px] font-medium">
+            <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-lg bg-[#857d75] p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none z-30 border-4 border-neutral-600">
+                <Dialog.Title className="text-neutral-100 m-0 text-[17px] font-medium">
                     Welcome to 0xVenture Capitalist
                 </Dialog.Title>
-                <Dialog.Description className="text-mauve11 mt-[10px] mb-5 text-[15px] leading-normal">
+                <Dialog.Description className="text-neutral-300 mt-[10px] mb-5 text-[15px] leading-normal">
                     Sign in or create an account to get started!
                 </Dialog.Description>
                 <button
                     onClick={() => signIn('google')}
-                    className="bg-white border text-black hover:bg-gray-100 hover:border-gray-400 hover:shadow-md inline-flex h-[35px] items-center justify-center rounded-[4px] px-[15px] font-medium leading-none w-full text-sm"
+                    className={`
+                        ${commonButtonClass}
+                    `}
                 >
                     <div className="absolute left-10">
                         <FcGoogle />
@@ -28,7 +32,10 @@ const RegisterModal = ({ children }: { children: React.ReactNode }) => (
                 </button>
                 <button
                     onClick={() => signIn('twitter')}
-                    className="bg-white border text-black hover:bg-gray-100 hover:border-gray-400 hover:shadow-md inline-flex h-[35px] items-center justify-center rounded-[4px] px-[15px] font-medium leading-none w-full text-sm mt-[1rem]"
+                    className={`
+                        ${commonButtonClass}
+                        mt-[1rem]
+                    `}
                 >
                     <div className="absolute left-10">
                         <TwitterLogoIcon />
@@ -37,7 +44,10 @@ const RegisterModal = ({ children }: { children: React.ReactNode }) => (
                 </button>
                 <button
                     onClick={() => signIn('github')}
-                    className="bg-white border text-black hover:bg-gray-100 hover:border-gray-400 hover:shadow-md inline-flex h-[35px] items-center justify-center rounded-[4px] px-[15px] font-medium leading-none w-full text-sm mt-[1rem]"
+                    className={`
+                        ${commonButtonClass}
+                        mt-[1rem]
+                    `}
                 >
                     <div className="absolute left-10">
                         <GitHubLogoIcon />
